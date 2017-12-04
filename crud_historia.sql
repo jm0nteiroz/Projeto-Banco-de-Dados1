62824a156959a84dff51e686a5e8acd8ba48f41f
ANDERSON AUGUSTO KURZ <a1305921@debian9-dacom.dacom>
2017-12-04 19:46:34 -0200

    bd update

--- crud_historia.sql
+++ crud_historia.sql
@@ -1,6 +1,6 @@
 INSERT INTO HISTORIA (ISBN, Nome) VALUES ({int}, {string});
 
-SELECT Nome FROM HISTORIA WHERE ISBN = {string};
+SELECT ISBN FROM HISTORIA WHERE Nome = {string};
 
 UPDATE HISTORIA SET Nome={string} WHERE ISBN = {int};
 
